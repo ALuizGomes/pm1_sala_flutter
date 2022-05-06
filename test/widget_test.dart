e514ed7eb5ca05ee6fde0aff5274f6d58b1fc186
Andre Luiz Gomes <201920378@unifoa.edu.br>
2022-05-06 10:41:43 -0300

    Front Page
    Inicio da front page. Nada muito especifico

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pm1_sala_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Review1());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
