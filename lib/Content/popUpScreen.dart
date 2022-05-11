08a8bf6e397410b86c4be2b36ce2601f573287ce
Andre Luiz Gomes <201920378@unifoa.edu.br>
2022-05-11 10:20:10 -0300

    Update popUpScreen.dart

--- lib/Content/popUpScreen.dart
+++ lib/Content/popUpScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PopUpScreen extends StatefulWidget {
   const PopUpScreen({ Key? key }) : super(key: key);
@@ -11,10 +12,23 @@ class _PopUpScreenState extends State<PopUpScreen> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      content: Column(
+        children: const [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Nome:',
+              border: OutlineInputBorder()
+            ),
+          )
+        ],
+      ),
       title: Center(
-        child: Text('Adicionar Usuario')
+        child: SizedBox(
+          child: Text('Adicionar Usuario')
+          )
         ),
-      // titlePadding: EdgeInsets.all(10),
+      titlePadding: EdgeInsets.all(10),
+      // titleTextStyle: TextStyle(),
     );
   }
 }
